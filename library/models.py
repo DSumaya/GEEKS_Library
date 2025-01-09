a5b463391f8a4d114e7ba1772a0b8a05e2a9142c
DSumaya <summa320808@gmail.com>
2025-01-09 16:11:08 +0600

    hw_2

--- library/models.py
+++ library/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Books(models.Model):
+    GENRE_CHOICES = (
+        ('Ужасы', 'Ужасы'),
+        ('Романтика', 'Романтика'),
+        ('Фэнтэзи', 'Фэнтэзи')
+    )
+    image = models.ImageField(upload_to = 'books/', verbose_name= 'Add photo')
+    title = models.CharField(max_length= 100, verbose_name= 'Add title for book')
+    description = models.TextField(verbose_name='Add description', blank= True)
+    price = models.PositiveIntegerField(verbose_name= 'Add price', default= 500)
+    release_date = models.DateTimeField(auto_now_add= True)
+    genre = models.CharField(max_length= 100, choices=GENRE_CHOICES, verbose_name='Add genre')
+    mail_autor = models.EmailField(verbose_name='Add mail autor')
+    autor = models.CharField(max_length=100, verbose_name='Add autor', default='Джоан Роулинг')
+    trailer = models.URLField(verbose_name='Add link from Youtube')
+    class Meta:
+        verbose_name = 'книгу'
+        verbose_name_plural = 'список книг'
+
+    def __str__(self):
+        return f'{self.title} : {self.price} сом'
