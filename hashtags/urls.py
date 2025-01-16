bb4239fa5ea822f53dee2f5b2eac9a93f25db313
DSumaya <summa320808@gmail.com>
2025-01-16 23:23:36 +0600

    html шаблоны

--- hashtags/urls.py
+++ hashtags/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 
 urlpatterns = [
-
+    path('all_books/', views.all_books, name= 'all_books'),
+    path('all_tale/',views.all_tale, name='all_tale'),
+    path('all_fantasy/', views.all_fantasy, name='all_fantasy'),
+    path('all_drama/', views.all_drama, name='all_drama'),
 ]
--- hashtags/views.py
+++ hashtags/views.py
@@ -5,7 +5,7 @@ from .import models
 #all
 def all_books(request):
     if request.method == 'GET':
-        all_books = models.Books.object.all()
+        all_books = models.Books.objects.all()
         context = {'all_books' : all_books}
         return render(request, template_name='hashtags/all_books.html', context= context)
 
@@ -14,20 +14,20 @@ def all_books(request):
 
 def all_tale(request):
     if request.method == 'GET':
-        all_tale = models.Books.object.filter(tags__name='Сказки')
+        all_tale = models.Books.objects.filter(tags__name='Сказки')
         context = {'all_tale' : all_tale}
         return render(request, template_name='hashtags/all_tale.html', context=context)
 
 
 def all_fantasy(request):
     if request.method == 'GET':
-        all_fantasy = models.Books.object.filter(tags__name='Фэнтэзи')
+        all_fantasy = models.Books.objects.filter(tags__name='Фантастика')
         context = {'all_fantasy' : all_fantasy}
         return render(request, template_name='hashtags/all_fantasy.html', context=context)
 
 
 def all_drama(request):
     if request.method == 'GET':
-        all_drama = models.Books.object.filter(tags__name='Сказки')
+        all_drama = models.Books.objects.filter(tags__name='Драма')
         context = {'all_drama' : all_drama}
         return render(request, template_name='hashtags/all_drama.html', context=context)
